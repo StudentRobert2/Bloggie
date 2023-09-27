70bc969d070e48b12251083e9285ddeb885844fa
Robert <karapetyanRobert0223@gmail.com>
2023-09-27 17:16:42 +0400

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,39 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext )
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+
+        [HttpGet] // սա ստացող մեթոդ է
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] // սա գրանցման մեթոդ է
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+            return View("Add");
+        }
+    }
+}
---
+++ Bloggie.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddTagRequest // Ավելացնել պիտակի հարցում
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+    }
+}
